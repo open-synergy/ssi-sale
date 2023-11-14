3b0d0f4ccf456af5d49d6f6c2b4a3a72e7886526
Miftahussalam <miftah_salam@ymail.com>
2023-11-14 23:15:57 +0700

    [ADD] ssi_sale

---
+++ ssi_sale/models/sale_order.py
@@ -0,0 +1,20 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    type_id = fields.Many2one(
+        comodel_name='sale_order_type',
+        string='Type',
+        required=True,
+        readonly=True,
+        states={
+            "draft": [("readonly", False)],
+        },
+    )
