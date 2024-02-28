3fe83938718d0d2f6e19943502f3d6706f0b713d
mikevhe18 <viriyananda.michael@gmail.com>
2024-02-28 15:43:03 +0700

    [FIX] pre-commit

--- ssi_sale/models/sale_order_type.py
+++ ssi_sale/models/sale_order_type.py
@@ -2,7 +2,7 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class SaleOrderType(models.Model):
